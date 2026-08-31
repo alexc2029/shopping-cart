85dbdb9484fdb783700eaa5f4f51bb6018fc4ee3
alexc2029 <alex2akec@gmail.com>
2026-08-31 15:36:34 +0300

    feat: move fetching to Shop and render products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router";
-import useProducts from "./hooks/useProducts";
 
 function App() {
-	const { products, error, loading } = useProducts();
 	return (
 		<>
 			<Navbar />
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,1 +1,14 @@
-export default function Shop() {}
+import ProductShopCard from "../components/ProductShopCard";
+import useProducts from "../hooks/useProducts";
+
+export default function Shop() {
+	const { products, error, loading } = useProducts();
+	return products.map((product) => (
+		<ProductShopCard
+			title={product.title}
+			price={product.price}
+			image={product.image}
+			key={product.id}
+		/>
+	));
+}

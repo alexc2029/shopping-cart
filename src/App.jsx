68377ac1111e418e4aee10e51ebe70a6d3c43168
Alexandru Ciogu <alex2akec@gmail.com>
2026-08-31 11:38:31 +0300

    feat: begin scaffolding app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router";
 
 function App() {
 	return (
 		<>
-			<h1>Hello world!</h1>
+			<Navbar />
+			<main>
+				<Outlet />
+			</main>
 		</>
 	);
 }

774c8e5a329973eb2ebf31fa6f9570a2e7e15f74
alexc2029 <alex2akec@gmail.com>
2026-08-31 15:38:09 +0300

    feat: render loading and error states for products

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,6 +3,8 @@ import useProducts from "../hooks/useProducts";
 
 export default function Shop() {
 	const { products, error, loading } = useProducts();
+	if (loading) return <p>Loading...</p>;
+	else if (error) return <p> Failed to load products.</p>;
 	return products.map((product) => (
 		<ProductShopCard
 			title={product.title}

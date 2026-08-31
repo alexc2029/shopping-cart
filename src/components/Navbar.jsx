382d88406d25713f1ff621083c99b1654ce4605b
Alexandru Ciogu <alex2akec@gmail.com>
2026-08-31 11:59:25 +0300

    feat: set up routes and navbar for main pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,11 @@
-export default function Navbar() {}
+import { NavLink } from "react-router";
+
+export default function Navbar() {
+	return (
+		<nav>
+			<NavLink to="/">Home</NavLink>
+			<NavLink to="/shop">Shop</NavLink>
+			<NavLink to="/cart">Cart</NavLink>
+		</nav>
+	);
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,26 @@
 import App from "./App";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 const routes = [
 	{
 		path: "/",
 		element: <App />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "shop",
+				element: <Shop />,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+		],
 	},
 ];
 

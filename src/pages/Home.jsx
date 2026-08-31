aa08ff5aaa3ff00ac68126669903ee278d2440e3
Alexandru Ciogu <alex2akec@gmail.com>
2026-08-31 12:46:05 +0300

    feat: add dummy homepage content and shop redirect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,12 @@
-export default function Home() {}
+import { Link } from "react-router";
+
+export default function Home() {
+	return (
+		<>
+			<h1>Welcome to Unknown Shop</h1>
+			<Link to="/shop">
+				<button>Shop Now</button>
+			</Link>
+		</>
+	);
+}

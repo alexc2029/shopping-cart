8d57539a7617685262d41cd1fd340b9957241da8
Alexandru Ciogu <alex2akec@gmail.com>
2026-09-01 11:12:22 +0300

    feat: add product count state and set default to 1

--- src/components/ProductShopCard.jsx
+++ src/components/ProductShopCard.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 export default function ProductShopCard({ title, price, imageUrl }) {
+	const [count, setCount] = useState(1);
 	return (
 		<div>
 			<img src={imageUrl} alt="" data-testid="product-image" />
 			<h2>{title}</h2>
 			<span>${price}</span>
-			<input type="number" />
+			<input
+				type="number"
+				value={count}
+				onChange={(e) => setCount(e.target.value)}
+			/>
 			<button>+</button>
 			<button>-</button>
 			<button>Add To Cart</button>

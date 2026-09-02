fdd8c18f68b4c6a92d7c15026f938f4db85300e5
Alexandru Ciogu <alex2akec@gmail.com>
2026-09-02 12:13:10 +0300

    refactor: extract components from ProductShopCard

--- src/components/ProductShopCard.jsx
+++ src/components/ProductShopCard.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import ProductShopCardActions from "./ProductShopCardActions";
+import ProductShopCardInfo from "./ProductShopCardInfo";
 
 export default function ProductShopCard({
 	title,
@@ -6,27 +7,14 @@ export default function ProductShopCard({
 	imageUrl,
 	onAddToCart,
 }) {
-	const [count, setCount] = useState(1);
-	const countAsNumber = Number(count);
 	return (
 		<div>
-			<img src={imageUrl} alt="" data-testid="product-image" />
-			<h2>{title}</h2>
-			<span>${price}</span>
-			<input
-				type="number"
-				value={count}
-				onChange={(e) => setCount(e.target.value)}
+			<ProductShopCardActions onAddToCart={onAddToCart} />
+			<ProductShopCardInfo
+				title={title}
+				imageUrl={imageUrl}
+				price={price}
 			/>
-			<button onClick={() => setCount(countAsNumber + 1)}>+</button>
-			<button
-				onClick={() => {
-					if (countAsNumber > 1) setCount(countAsNumber - 1);
-				}}
-			>
-				-
-			</button>
-			<button onClick={() => onAddToCart(count)}>Add To Cart</button>
 		</div>
 	);
 }
---
+++ src/components/ProductShopCardActions.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function ProductShopCardActions({ onAddToCart }) {
+	const [count, setCount] = useState(1);
+	const countAsNumber = Number(count);
+	return (
+		<div>
+			<input
+				type="number"
+				value={count}
+				onChange={(e) => setCount(e.target.value)}
+			/>
+			<button onClick={() => setCount(countAsNumber + 1)}>+</button>
+			<button
+				onClick={() => {
+					if (countAsNumber > 1) setCount(countAsNumber - 1);
+				}}
+			>
+				-
+			</button>
+			<button onClick={() => onAddToCart(count)}>Add To Cart</button>
+		</div>
+	);
+}

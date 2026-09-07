0ac6ead22989156e5b431e6745831236e22f8ac6
Alexandru Ciogu <alex2akec@gmail.com>
2026-09-07 15:00:57 +0300

    test: add tests for + and - shop quantity button functionality

--- src/components/shop/ShopProduct.test.jsx
+++ src/components/shop/ShopProduct.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import ShopProduct from "./ShopProduct";
+import userEvent from "@testing-library/user-event";
 
 describe("ShopProduct tests", () => {
 	it("renders product info", () => {
@@ -30,5 +31,20 @@ describe("ShopProduct tests", () => {
 		expect(screen.getByRole("button", { name: "+" })).toBeInTheDocument();
 		expect(screen.getByRole("button", { name: "-" })).toBeInTheDocument();
 		expect(screen.getByRole("spinbutton")).toHaveDisplayValue(1);
+	});
+	it("increments and decrements quantity through + and - buttons", async () => {
+		render(<ShopProduct onAddToCart={() => {}} />);
+		const user = userEvent.setup();
+		const plusButton = screen.getByRole("button", { name: "+" });
+		const minusButton = screen.getByRole("button", { name: "-" });
+		const quantityInput = screen.getByRole("spinbutton");
+
+		await user.click(plusButton);
+
+		expect(quantityInput).toHaveDisplayValue(2);
+
+		await user.click(minusButton);
+
+		expect(quantityInput).toHaveDisplayValue(1);
 	});
 });

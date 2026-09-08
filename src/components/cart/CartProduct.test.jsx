2315020b0fef792bcf671d6b3955667de5ee132c
Alexandru Ciogu <alex2akec@gmail.com>
2026-09-08 13:09:38 +0300

    test: add tests for + and - cart quantity buttons

--- src/components/cart/CartProduct.test.jsx
+++ src/components/cart/CartProduct.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import CartProduct from "./CartProduct";
+import { useState } from "react";
 
 describe("CartProduct tests", () => {
 	it("renders product info", () => {
@@ -29,5 +30,29 @@ describe("CartProduct tests", () => {
 		render(<CartProduct count="3" />);
 
 		expect(screen.getByTestId("quantity-display")).toHaveTextContent(3);
+	});
+	function QuantityWrapper() {
+		const [count, setCount] = useState(1);
+
+		const updateCount = (clickedCount) => {
+			setCount(count + clickedCount);
+		};
+
+		return <CartProduct count={count} onUpdateCount={updateCount} />;
+	}
+	it("increments and decrements quantity through + and - buttons", async () => {
+		render(<QuantityWrapper />);
+		const user = userEvent.setup();
+		const plusButton = screen.getByRole("button", { name: "+" });
+		const minusButton = screen.getByRole("button", { name: "-" });
+		const quantityDisplay = screen.getByTestId("quantity-display");
+
+		await user.click(plusButton);
+
+		expect(quantityDisplay).toHaveTextContent(2);
+
+		await user.click(minusButton);
+
+		expect(quantityDisplay).toHaveTextContent(1);
 	});
 });

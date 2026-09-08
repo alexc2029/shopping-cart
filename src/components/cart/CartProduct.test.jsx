0d57b0be880c722ff7862322fdc97bef19eeb284
Alexandru Ciogu <alex2akec@gmail.com>
2026-09-08 12:35:03 +0300

    test: add tests for cart product info rendering

---
+++ src/components/cart/CartProduct.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import CartProduct from "./CartProduct";
+
+describe("CartProduct tests", () => {
+	it("renders product info", () => {
+		const mockProps = {
+			title: "Backpack",
+			price: "19",
+			imageUrl: "http://example.com",
+		};
+		render(
+			<CartProduct
+				title={mockProps.title}
+				price={mockProps.price}
+				imageUrl={mockProps.imageUrl}
+			/>,
+		);
+
+		expect(screen.getByText(mockProps.title)).toBeInTheDocument();
+		expect(screen.getByText(`$${mockProps.price}`)).toBeInTheDocument();
+		expect(screen.getByTestId("product-image")).toHaveAttribute(
+			"src",
+			mockProps.imageUrl,
+		);
+	});
+});

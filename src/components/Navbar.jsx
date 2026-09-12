2e95166a8d6ab03ab268987a12860f027a2ecd1b
alexc2029 <alex2akec@gmail.com>
2026-09-12 14:23:33 +0300

    feat: wrap navbar elements for layout and add site name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
-import { NavLink } from "react-router";
+import { NavLink, Link } from "react-router";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const NavLinksWrapper = styled.div`
+	display: flex;
+	gap: 1rem;
+`;
 
 export default function Navbar({ productsInCartCount }) {
 	return (
-		<nav>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/shop">
-				{productsInCartCount > 0 && (
-					<span
-						aria-label={`${productsInCartCount} products in cart`}
-						data-testid="cart-badge"
-					>
-						{productsInCartCount}
-					</span>
-				)}
-				Shop
-			</NavLink>
-			<NavLink to="/cart">Cart</NavLink>
-		</nav>
+		<StyledNav>
+			<Link to="/">Lorem & Ipsum Co.</Link>
+			<NavLinksWrapper>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/shop">
+					{productsInCartCount > 0 && (
+						<span
+							aria-label={`${productsInCartCount} products in cart`}
+							data-testid="cart-badge"
+						>
+							{productsInCartCount}
+						</span>
+					)}
+					Shop
+				</NavLink>
+				<NavLink to="/cart">Cart</NavLink>
+			</NavLinksWrapper>
+		</StyledNav>
 	);
 }

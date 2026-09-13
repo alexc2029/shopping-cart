f06ee08c06b4a2d7bbd0635bc89c0f70440ccb26
alexc2029 <alex2akec@gmail.com>
2026-09-13 13:14:51 +0300

    feat: improve shop product info layout

--- src/components/shop/ShopProductInfo.jsx
+++ src/components/shop/ShopProductInfo.jsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+
+const StyledProductInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 5px;
+	& img {
+		max-width: 100%;
+		height: 30vh;
+	}
+`;
+
 export default function ShopProductInfo({ title, imageUrl, price }) {
 	return (
-		<div>
+		<StyledProductInfo>
 			<img src={imageUrl} alt="" data-testid="product-image" />
 			<h2>{title}</h2>
 			<span>${price}</span>
-		</div>
+		</StyledProductInfo>
 	);
 }

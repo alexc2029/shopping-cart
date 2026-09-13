76e6d57c60e18d4b13b44f3f3f5d431e30c458eb
alexc2029 <alex2akec@gmail.com>
2026-09-13 13:16:00 +0300

    feat: add grid layout to shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,21 +1,32 @@
+import styled from "styled-components";
 import ShopProduct from "../components/shop/ShopProduct";
 import useProducts from "../hooks/useProducts";
 import { useOutletContext } from "react-router";
+
+const ShopLayout = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+	gap: 3vw;
+`;
 
 export default function Shop() {
 	const { products, error, loading } = useProducts();
 	const { addProductToCart } = useOutletContext();
 	if (loading) return <p>Loading...</p>;
 	else if (error) return <p>Failed to load products.</p>;
-	return products.map((product) => (
-		<ShopProduct
-			title={product.title}
-			price={product.price}
-			imageUrl={product.image}
-			key={product.id}
-			onAddToCart={(count) =>
-				addProductToCart({ ...product, count: count })
-			}
-		/>
-	));
+	return (
+		<ShopLayout>
+			{products.map((product) => (
+				<ShopProduct
+					title={product.title}
+					price={product.price}
+					imageUrl={product.image}
+					key={product.id}
+					onAddToCart={(count) =>
+						addProductToCart({ ...product, count: count })
+					}
+				/>
+			))}
+		</ShopLayout>
+	);
 }

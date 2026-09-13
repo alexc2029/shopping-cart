a521a9da846f2cb5986ac08d7bed1400039ba386
alexc2029 <alex2akec@gmail.com>
2026-09-13 15:46:05 +0300

    feat: lay out shop actions and reset count input style

--- src/components/shop/ShopProductActions.jsx
+++ src/components/shop/ShopProductActions.jsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const StyledShopActions = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 5px;
+	margin-top: 10px;
+`;
+
+const StyledShopCount = styled.div``;
+
+const CountInput = styled.input`
+	width: 2rem;
+
+	/* Firefox */
+	appearance: textfield;
+	-moz-appearance: textfield;
+
+	/* Chrome, Safari, Edge, Opera */
+	&::-webkit-outer-spin-button,
+	&::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+`;
 
 export default function ShopProductActions({ onAddToCart }) {
 	const [count, setCount] = useState(1);
 	const countAsNumber = Number(count);
 	return (
-		<div>
-			<input
-				type="number"
-				value={count}
-				onChange={(e) => setCount(e.target.value)}
-			/>
-			<button onClick={() => setCount(countAsNumber + 1)}>+</button>
-			<button
-				onClick={() => {
-					if (countAsNumber > 1) setCount(countAsNumber - 1);
-				}}
-			>
-				-
-			</button>
+		<StyledShopActions>
+			<StyledShopCount>
+				<button
+					onClick={() => {
+						if (countAsNumber > 1) setCount(countAsNumber - 1);
+					}}
+				>
+					-
+				</button>
+				<CountInput
+					type="number"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button onClick={() => setCount(countAsNumber + 1)}>+</button>
+			</StyledShopCount>
 			<button onClick={() => onAddToCart(count)}>Add To Cart</button>
-		</div>
+		</StyledShopActions>
 	);
 }

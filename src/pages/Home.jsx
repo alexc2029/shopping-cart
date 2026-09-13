73bf11ce4b0adea8405cfb8f4258e4c7543f333c
alexc2029 <alex2akec@gmail.com>
2026-09-13 12:50:51 +0300

    feat: style heading, subheading and CTA in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,47 @@
 import { Link } from "react-router";
+import styled from "styled-components";
+
+const Ampersand = styled.span`
+	font-family: Georgia, Helvetica, sans-serif;
+`;
+
+const StyledHeading1 = styled.h1`
+	font-size: 4rem;
+	font-family: "PlayfairDisplay", Helvetica, sans-serif;
+`;
+
+const StyledHeading2 = styled.h2`
+	color: var(--color-muted);
+	font-weight: normal;
+`;
+
+const StyledLink = styled(Link)`
+	text-decoration: none;
+	background-color: var(--color-accent);
+	color: white;
+	border-radius: 8px;
+	padding: 12px 28px;
+`;
+
+const HomeLayout = styled.div`
+	margin-top: 5rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 20px;
+`;
 
 export default function Home() {
 	return (
-		<>
-			<h1>Welcome to Unknown Shop</h1>
-			<Link to="/shop">Shop Now</Link>
-		</>
+		<HomeLayout>
+			<StyledHeading1>
+				Welcome to Lorem <Ampersand>&</Ampersand> Ipsum Co.
+			</StyledHeading1>
+			<StyledHeading2>
+				An exquisite collection of products that will never arrive,
+				charge your card, or take up closet space.
+			</StyledHeading2>
+			<StyledLink to="/shop">Browse the Catalog</StyledLink>
+		</HomeLayout>
 	);
 }

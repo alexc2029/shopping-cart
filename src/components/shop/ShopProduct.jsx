4a815ac8e78a8ad5b285479aaffd7e0c3e603820
alexc2029 <alex2akec@gmail.com>
2026-09-13 17:28:23 +0300

    feat: improve shop product card styling

--- src/components/shop/ShopProduct.jsx
+++ src/components/shop/ShopProduct.jsx
@@ -1,11 +1,22 @@
+import styled from "styled-components";
 import ShopProductActions from "./ShopProductActions";
 import ShopProductInfo from "./ShopProductInfo";
 
+const StyledShopProduct = styled.div`
+	background-color: var(--color-surface);
+	border: 1px solid var(--color-border);
+	border-radius: 8px;
+	padding: 16px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
 export default function ShopProduct({ title, price, imageUrl, onAddToCart }) {
 	return (
-		<div>
-			<ShopProductActions onAddToCart={onAddToCart} />
+		<StyledShopProduct>
 			<ShopProductInfo title={title} imageUrl={imageUrl} price={price} />
-		</div>
+			<ShopProductActions onAddToCart={onAddToCart} />
+		</StyledShopProduct>
 	);
 }

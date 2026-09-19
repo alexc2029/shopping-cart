00320eb052f65312085a7bcdf748c5232fd04b75
alexc2029 <alex2akec@gmail.com>
2026-09-19 18:12:34 +0300

    feat: add initial cart product info styling

--- src/components/cart/CartProductInfo.jsx
+++ src/components/cart/CartProductInfo.jsx
@@ -1,9 +1,32 @@
+import styled from "styled-components";
+
+const StyledProductInfo = styled.div`
+	display: flex;
+	& img {
+		height: 80px;
+		width: 80px;
+	}
+`;
+
+const TitleAndPrice = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Title = styled.h2`
+	font-weight: normal;
+	letter-spacing: 0.5px;
+	font-size: 1.2rem;
+`;
+
 export default function CardProductInfo({ title, imageUrl, price }) {
 	return (
-		<div>
+		<StyledProductInfo>
 			<img src={imageUrl} alt="" data-testid="product-image" />
-			<h2>{title}</h2>
-			<span>${price}</span>
-		</div>
+			<TitleAndPrice>
+				<Title>{title}</Title>
+				<span>${price}</span>
+			</TitleAndPrice>
+		</StyledProductInfo>
 	);
 }
